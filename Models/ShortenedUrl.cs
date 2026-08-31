b228bbf9390b9cfb47bb1f551ef8cc6b9e011827
jonaferreir4 <jonaferreira2204@gmail.com>
2026-08-31 12:18:33 -0300

    feat: add additional properties to ShortenedUrl model for enhanced functionality

--- Models/ShortenedUrl.cs
+++ Models/ShortenedUrl.cs
@@ -7,8 +7,11 @@ public class ShortenedUrl
     public string ShortUrl { get; set; } = string.Empty;
     public string Code { get; set; } = string.Empty;
     public int Click { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime? ExpiresAtUtc { get; set; }
+    public int? MaxClicks { get; set; }
+    public bool IsCustom { get; set; }
     public DateTime CreatedOnUtc { get; set; }
 
     public ICollection<AccessLog> AccessLogs { get; set; } = [];
-   
 }
